d6b3aad69a2c191acaf01dcb6ab843af252f0dc9
Aaron Ekal <ekalaaronkalale@gmail.com>
2022-03-04 00:19:46 +0300

    added switch statement

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+let myGrade = 'Z'
+switch (myGrade){
+    case 'A':
+        console.log("You did graet!")
+        break;
+
+    case 'B':
+        console.log('You did good!')
+        break
+
+    case 'C':
+        console.log('You did good!')
+        break
+
+    case 'D':
+        console.log('improve next!')
+        break
+
+    case 'F':
+        console.log('You Failed')
+        break
+
+    default:
+        console.log(myGrade,'Invalid grade')
+    
+}
